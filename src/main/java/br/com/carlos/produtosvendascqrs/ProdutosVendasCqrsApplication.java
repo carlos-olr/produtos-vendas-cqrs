ebc16a46675d0979b8a5dd32b2efa2e03ca6c069
Carlos Oliveira <carlos.oliveira@touchtec.com.br>
2021-06-16 05:55:58 -0300

    inicial

---
+++ src/main/java/br/com/carlos/produtosvendascqrs/ProdutosVendasCqrsApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlos.produtosvendascqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosVendasCqrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosVendasCqrsApplication.class, args);
+	}
+
+}
